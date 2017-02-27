bcf4b3db1e909b082d9b060ca78aa56630b5732a
Erik Enderlein <erik.end@gmail.com>
2017-02-27 10:45:24 -0600

    RNG to create a winner for an NPC fight created. Also system to set up the next round of a tournament is created.

--- js/tourny.js
+++ js/tourny.js
@@ -14,4 +14,37 @@ function setUpMatches(){
     console.log(chosen);
   }
 }
+function npcFight(fighterA, fighterB){
+  var winSelect = Math.floor(Math.random() * 2);
+  if (winSelect === 0){
+    console.log('Fighter A wins!');
+    return fighterA;
+  } else if (winSelect === 1){
+    console.log('Fighter B wins!');
+    return fighterB;
+  }
+}
+
+function tournamentRound(){
+  var winners = [];
+  var roundLength = chosen.length / 2;
+  for (var i = 0; i < roundLength; i++){
+    var nextFight = [];
+    while (nextFight.length < 2){
+      nextFight.push(chosen.shift());
+    }
+    console.log('next fight is ' + nextFight);
+    var winner = npcFight(nextFight[0], nextFight[1]);
+    winners.push(winner);
+  }
+  console.log(winners);
+  chosen = winners;
+}
+
+function tournament(){
+  setUpMatches();
+}
 setUpMatches();
+tournamentRound();
+tournamentRound();
+tournamentRound();
