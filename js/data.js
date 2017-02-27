ef25e962e7066ce560019b206eb9dd39c3c1833a
Enrique Rico <enriquesantosrico@gmail.com>
2017-02-27 10:25:45 -0800

    added imgs folder for character imgs and created to working charcaters for testing

---
+++ js/data.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var teddyQ1 = ['Roosevelt\'s Rough riders famously fought in:', 'Cuba', 'Texas', 'Mexico', 'Pananma'];
+
+var teddyQ2 = ['Roosevelt attended which college:', 'Harvard', 'Yale', 'Oxford', 'Cornell'];
+
+var teddyQ3 = ['Both Roosevelts\'s wife and mother famously:', 'Died on the same day', 'Had the same name', 'Hatd each other', 'Were second cousins'];
+
+var teddyQ4 = ['Teddy Roosevelt is the only President in American history to:', 'Win both the Medal of Honor and the Nobel Peace Prize', 'Fire his entire security cabinet on the same day', 'Survive four separate public assassination attempts', 'Shoot someone while in office'];
+
+// ADD questions above teddy var
+
+var teddy = ['Teddy Roosevelt', 'img/Teddy.jpg', teddyQ1, teddyQ2, teddyQ3, teddyQ4, teddyQ5, teddyQ6, teddyQ7, teddyQ8, teddyQ9, teddyQ10, teddyQ11, teddyQ12];
+
+///
+///
+///
+///
+///
+///
+
+var daVinciQ1 = ['What\'s the full name of Da Vinci\'s famous painting, "Mona ____"', 'Lisa', 'Elizabeth', 'the girl next door', 'the disney princess'];
+
+var daVinciQ2 = ['Which one of these famous works of art was created by Da Vinci?','The Last Supper', 'Nighthawks', 'The Creation of Adam', 'Portrait of a Young Man'];
+
+var daVinciQ3 = ['Where was Leonardo born?','Florence', 'Milan', 'Rome', 'Venice'];
+
+var daVinciQ4 = ['How many siblings did Da Vinci have?','17', '5', '0', '7'];
+//
+//ADD questions for daVinci var before this daVinci Var
+
+var daVinci = ['Leonardo Da Vinci','img/leonardo-da-vinci.jpg', daVinciQ1, daVinciQ2, daVinciQ3, daVinciQ4, daVinciQ5, daVinciQ6, daVinciQ7, daVinciQ8, daVinciQ9, daVinciQ10, daVinciQ11, daVinciQ12];
+
+//
+//
+//
+//
