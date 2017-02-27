4c1d40847c0b199e44a391001b503bc1741e4699
Enrique Rico <enriquesantosrico@gmail.com>
2017-02-27 14:13:19 -0800

    added 5 more characters with questions and answers.

--- js/data.js
+++ js/data.js
@@ -90,3 +90,156 @@ var ghandiQ11 = ['Which book was worked on by Ghandi?', 'Light of India', 'The l
 var ghandiQ12 = ['Ghandi confessed his guilt for _____. Promising to never do the act again.', 'Stealing', 'Drinking', 'Setting random fires', 'Having an Affair'];
 
 var ghandi = ['Mahatma Ghandi', 'IMG URL', ghandiQ1, ghandiQ2, ghandiQ3, ghandiQ4, ghandiQ5, ghandiQ6, ghandiQ7, ghandiQ8, ghandiQ9, ghandiQ10, ghandiQ11, ghandiQ12];
+///
+/// Ghandi Above!
+
+var darwinQ1 = ['What role did Charles Darwin play in biology?', 'He created the theory on The Origin of Species', 'He made the first bacterial pie', 'He discovered the DNA structure' , 'He discovered the mitochondria'];
+
+var darwinQ2 = ['What year  was Charles Darwin born in?', '1809', '1908', '1882', '1820'];
+
+var darwinQ3 = ['What is Natural Selection?', 'The process in which creatures are developed by random mutations over long periods of time (millions of years)', 'Making a decision by yourself, without any outer influence', 'The science of creating an organic garden', 'Ingredients on how to make a pie, that are found in the wild'];
+
+var darwinQ4 = ['The title of Charles Darwin original published book was?', 'On the Origin of Species', 'The Bible of Biology', 'Evolution and the creation of the world', 'The Origin of Species'];
+
+var darwinQ5 = ['At the University of Edinburgh, Darwin studied to become a______?', 'Physician', 'Biologist', 'Clergyman', 'Naturalist'];
+
+var darwinQ6 = ['What hobby did Darwin pick up while at the University of Edinburgh?', 'Beetle Collecting', 'Beer Pong', 'Hunting', 'Bird Watching'];
+
+var darwinQ7 = ['What lowly species did Darwin spend eight years studying', 'Barnacles', 'Snails', 'Worms', 'Fish'];
+
+var darwinQ8 = ['What year did Darwin die?', '1882', '1872', '1863', '1971'];
+
+var darwinQ9 = ['At what age did Darwin begin to attend the Universty of Edinburgh?', '16', '17', '20', '28'];
+
+var darwinQ10 = ['How many children did Darwin have?', '10', '7', '1', 'none'];
+
+var darwinQ11 = ['What was Darwin\'s wifes name?', 'Emma', 'Loray', 'Rose', 'Amy'];
+
+var darwinQ12 = ['Darwin took a cruise on which ship?', 'The Beagle', 'The Tutton', 'The Baker', 'The Nashville'];
+//
+//
+//
+var darwin = ['Charles Darwin', 'IMG URL', darwinQ1, darwinQ2, darwinQ3, darwinQ4, darwinQ5, darwinQ6, darwinQ7, darwinQ8, darwinQ9, darwinQ10, darwinQ11, darwinQ12];
+/////
+/// Darwin Above!
+///
+var voltaireQ1 = ['How long was Voltaire in the Bastille?', '1 year', '2 years', '3 years', '58 years'];
+
+var voltaireQ2 = ['Why was Voltaire in the Bastille?', 'For his love of scandalous verse in Candide', 'Theft', 'His beauty was criminal in the eyes of the French nobility', 'He never went to the Bastille'];
+
+var voltaireQ3 = ['How did Voltaire become rich?', 'A loophole in the French lottery', 'Hard work', 'He wasn\'t rich', 'Family'];
+
+var voltaireQ4 = ['How many plays did Voltaire write?', 'Over 50', '30-50', '20-30', 'Less than 20'];
+
+var voltaireQ5 = ['How many hours a day is Voltaire said to have written?', '18', '20', '16', '25'];
+
+var voltaireQ6 = ['How many cups of coffee a day was Voltaire said to drink?', '40', '8', '3', '15'];
+
+var voltaireQ7 = ['Why did Voltaire flee to the French countryside?', 'He wrote Letters Concerning the English Nation', ' He wrote Candide and was critically panned', 'He was too beautiful to be seen by mere mortals', 'He did not flee, he was a courageous man'];
+
+var voltaireQ8 = ['How did Voltaire help Isaac Newton?', 'He helped make the apple story well known', 'He gave Isaac Newton a fruit basket with an apple in it', 'He discovered gravity and let Isaac take credit', 'He didn\'t because gravity isn\'t real'];
+
+var voltaireQ9 = ['In what way did Voltaire work for the French government?', 'As a spy', 'As a Jester', 'As a tactician', 'As a janitor'];
+
+var voltaireQ10 = ['How many times did Voltaire marry?', '0', '1', '2', '3'];
+
+var voltaireQ11 = ['Where did Voltaire die?', 'In his sleep', 'In church', 'Restaurant', 'In a Prussian prison'];
+
+var voltaireQ12 = ['Which of Voltaire\'s books was banned upon publishing?', 'Candide', 'Plato\'s Dream', 'Oedipus', 'Zaire'];
+//
+//
+//
+var voltaire = ['Voltaire', 'imgURL', voltaireQ1, voltaireQ2, voltaireQ3, voltaireQ4, voltaireQ5, voltaireQ6, voltaireQ7, voltaireQ8, voltaireQ9, voltaireQ10, voltaireQ11, voltaireQ12];
+//
+// Voltaire Above!
+//
+
+var joanOfArcQ1 = ['What is Joan of Arc\'s nickname?', 'The Maid of Orleans', 'The Maid of Arc', 'The Savior of Orleans', 'The Saint of Arc'];
+
+var joanOfArcQ2 = ['What war was Joan of Arc a part of?', 'The Hundred Years\' war', 'The Crusades', 'The Vietnam War', 'The Franco-Flemish War'];
+
+var joanOfArcQ3 = ['Joan of Arc had visions of which saint?', 'Saint Margaret', 'Saint Paul', 'Saint Jean', 'Saint James'];
+
+var joanOfArcQ4 = ['Which king was Joan of Arc told to support?', 'King Charles VII', 'King James', 'King Charles IV', 'King Louis XI'];
+
+var joanOfArcQ5 = ['Where was Joan of Arc captured in 1430?', 'Compiegne', 'Paris', 'Cannes', 'Orleans'];
+
+var joanOfArcQ6 = ['At what age did Joan of Arc die?', '19', '23', '25', '21'];
+
+var joanOfArcQ7 = ['How did Joan of Arc die?', 'Burned at the stake', 'Starvation', 'Old age', 'Hanging'];
+
+var joanOfArcQ8 = ['Who declared Joan of Arc a national symbol of France?', 'Napolean Bonaparte', 'King Charles VII', 'Pope Callixtus III', 'Pierre Cauchon'];
+
+var joanOfArcQ9 = ['What village was Joan of Arc born in?', 'Domremy', 'Paris', 'Orleans', 'Cannes'];
+
+var joanOfArcQ10 = ['At what age did Joan of Arc see her first vision?', '13', '15', '17', '19'];
+
+var joanOfArcQ11 = ['Joan of Arc was allowed into the military only after she what?', 'Predicted the outcome of a battle', 'Spoke of a man\'s dreams', 'Predicted the king\'s victory', 'Won a fight with a nobleman'];
+
+var joanOfArcQ12 = ['Joan of Arc was brought ot trial for what?', 'Heresy', 'War crimes', 'Being a woman', 'Killing an English nobleman'];
+//
+//
+//
+var joanOfArc = ['Joan of Arc', 'imgURL', joanOfArcQ1, joanOfArcQ2, joanOfArcQ3, joanOfArcQ4, joanOfArcQ5, joanOfArcQ6, joanOfArcQ7, joanOfArcQ8, joanOfArcQ9, joanOfArcQ10, joanOfArcQ11, joanOfArcQ12];
+/////
+/// Joan of Arc Above!!
+///
+//
+var maoZedongQ1 = ['What movement did Mao Zedong create in 1966?', 'The cultural revolution', 'The American revolution', 'The Chineses democracy movement', 'The May 4th movement'];
+
+var maoZedongQ2 = ['How did Mao Zedong die?', 'Heart attack', 'Old age', 'Poison', 'Gunshot'];
+
+var maoZedongQ3 = ['Which Presiont of the United States did Mao Zedong welcome in Beijing?', 'Richard Nixon', 'Lyndon B. Johnson', 'JFK', 'Jimmy Carter'];
+
+var maoZedongQ4 = ['Which province was Mao Zedong born in?', 'Hunan', 'Bejing', 'Tibet', 'Henan'];
+
+var maoZedongQ5 = ['Mao Zedong was a what?', 'Feminist', 'Vegan', 'Pacifist', 'Anti-Apartheid'];
+
+var maoZedongQ6 = ['At what age did Mao Zedong get betrothed to Luo Yigu?', '14', '16', '18', '20'];
+
+var maoZedongQ7 = ['Around what year did Mao Zedong learn of socialism?', '1912', '1911', '1910', '2016'];
+
+var maoZedongQ8 = ['When was the People\'s Republic of China established?', '1949', '1980', '1933', '2012'];
+
+var maoZedongQ9 = ['In January 1958, Mao Zedong initiated what plan?', 'The Great Leap Forward', 'The Sunflower Protests', 'The Six Year Plan', 'The Anti-Rightist Movement'];
+
+var maoZedongQ10 = ['Each person in the new party in China tried to curry favor by exaggerating their production numbers. This led to what?', 'The Great Chinese Famine', 'The Great Exodus', 'The Great Leap Forward', 'This didn\'t happen'];
+
+var maoZedongQ11 = ['Mao Zedong was given a gift by the Pakistani Foreign Minister in 1968. Giving this gift to propaganda teams resulted in what?', 'Mango fever', 'China\'s uprising', 'The Great Honor', 'The American Revolution'];
+
+var maoZedongQ12 = ['Mao Zedong had how many marriages?', '4', '1', '2', '3'];
+//
+//
+//
+var maoZedong = ['Mao Zedong', 'imgURL', maoZedongQ1, maoZedongQ2, maoZedongQ3, maoZedongQ4, maoZedongQ5, maoZedongQ6, maoZedongQ7, maoZedongQ8, maoZedongQ9, maoZedongQ10, maoZedongQ11, maoZedongQ12];
+//
+/// Mao Zedong Above!
+//
+//
+var edgarAllanPoeQ1 = ['Edgar Allan Poe was part of what artistic movement?', 'Romanticism', 'Macabreism', 'Realism', 'Abstractism'];
+
+var edgarAllanPoeQ2 = ['Edgar Allan Poe is one of the country\'s earliest practitioners of what?', 'Short stories', 'Novellas', 'Alcoholism', 'Pastel art'];
+
+var edgarAllanPoeQ3 = ['Edgar Allan Poe is considered the inventor of what genre?', 'Detective fiction', 'Spooky scary stories', 'Spooky scary poetry', 'Macabre'];
+
+var edgarAllanPoeQ4 = ['Edgar Allan Poe attended what school?', 'Universtiy of Virginia', 'University of Washington', 'MIT', 'CodeFellows'];
+
+var edgarAllanPoeQ5 = ['Edgar Allan Poe enlisted into which branch of the military?', 'Army', 'Navy', 'Marines', 'Air Force'];
+
+var edgarAllanPoeQ6 = ['There is a prominent theory that Edgar Allan Poe is what?', 'A time traveler', 'A killer', 'A master of Jiu Jitsu', 'Imaginary'];
+
+var edgarAllanPoeQ7 = ['Edgar Allan Poe had how many siblings?', '2', '3', '0', '4'];
+
+var edgarAllanPoeQ8 = ['What was Edgar Allan Poe\'s first published book?', 'Tamerlane', 'Boo', 'His collected short stories', 'Spooky Stories'];
+
+var edgarAllanPoeQ9 = ['Edgar Allan Poe married at what age?', '27', '13', '25', '18'];
+
+var edgarAllanPoeQ10 = ['Edgar Allan Poe married when his wife was what age?', '13', '25', '18', '27'];
+
+var edgarAllanPoeQ11 = ['Edgar Allan Poe\'s only novel is titled what?', 'The Narrative of Arthur Gordon Pym', 'Tamerlane', 'Ender\'s Game', 'Game of Thrones'];
+
+var edgarAllanPoeQ12 = ['Edgar Allan Poe\'s death was caused by what?', 'Unkown reasons', 'Alcohol poisoning', 'Hanging', 'Old Age'];
+//
+//
+
+var edgarAllanPoe = ['Edgar Allan Poe', 'imgURL', edgarAllanPoeQ1, edgarAllanPoeQ2, edgarAllanPoeQ3, edgarAllanPoeQ4, edgarAllanPoeQ5, edgarAllanPoeQ6, edgarAllanPoeQ7, edgarAllanPoeQ8, edgarAllanPoeQ9, edgarAllanPoeQ10, edgarAllanPoeQ11, edgarAllanPoeQ12];
