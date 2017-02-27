c0df9f0e8a8cd74ca8c490de0cb92cedf93a387c
Erik Enderlein <erik.end@gmail.com>
2017-02-27 12:24:52 -0600

    uncommented previous work

--- js/tourny.js
+++ js/tourny.js
@@ -22,58 +22,57 @@ generateCharacters();
 Character.prototype.Question = function(Questions){
 };
 
-//
-// function setUpMatches(){
-//   var chosen = [];
-//   while (chosen.length < tournySize){
-//     var selected = Math.floor(Math.random() * characters.length);
-//     if (chosen.includes(selected)){
-//       console.log('Already selected, rerolling...');
-//     } else {
-//       chosen.push(selected);
-//     }
-//     console.log(chosen);
-//   }
-//   return chosen;
-// }
-// function npcFight(fighterA, fighterB){
-//   var winSelect = Math.floor(Math.random() * 2);
-//   if (winSelect === 0){
-//     console.log('Fighter A wins!');
-//     return fighterA;
-//   } else if (winSelect === 1){
-//     console.log('Fighter B wins!');
-//     return fighterB;
-//   }
-// }
-//
-// function tournamentRound(contestents){
-//   var winners = [];
-//   var roundLength = contestents.length / 2;
-//   for (var i = 0; i < roundLength; i++){
-//     var nextFight = [];
-//     while (nextFight.length < 2){
-//       nextFight.push(contestents.shift());
-//     }
-//     console.log('next fight is ' + nextFight);
-//     var winner = npcFight(nextFight[0], nextFight[1]);
-//     winners.push(winner);
-//   }
-//   console.log(winners);
-//   return winners;
-// }
-//
-// function tournament(){
-//   var contestents = setUpMatches();
-//   if (tournySize === 8){
-//     var roundOneWinners = tournamentRound(contestents);
-//     var roundTwoWinners = tournamentRound(roundOneWinners);
-//     var finalWinner = tournamentRound(roundTwoWinners);
-//   } else if (tournySize === 16){
-//     var roundOneWinners = tournamentRound(contestents);
-//     var roundTwoWinners = tournamentRound(roundOneWinners);
-//     var roudThreeWinners = tournamentRound(roundTwoWinners);
-//     var finalWinner = tournamentRound(roundThreeWinners);
-//   }
-//   console.log('The final victor is: ' + finalWinner);
-// }
+function setUpMatches(){
+  var chosen = [];
+  while (chosen.length < tournySize){
+    var selected = Math.floor(Math.random() * characters.length);
+    if (chosen.includes(selected)){
+      console.log('Already selected, rerolling...');
+    } else {
+      chosen.push(selected);
+    }
+    console.log(chosen);
+  }
+  return chosen;
+}
+function npcFight(fighterA, fighterB){
+  var winSelect = Math.floor(Math.random() * 2);
+  if (winSelect === 0){
+    console.log('Fighter A wins!');
+    return fighterA;
+  } else if (winSelect === 1){
+    console.log('Fighter B wins!');
+    return fighterB;
+  }
+}
+
+function tournamentRound(contestents){
+  var winners = [];
+  var roundLength = contestents.length / 2;
+  for (var i = 0; i < roundLength; i++){
+    var nextFight = [];
+    while (nextFight.length < 2){
+      nextFight.push(contestents.shift());
+    }
+    console.log('next fight is ' + nextFight);
+    var winner = npcFight(nextFight[0], nextFight[1]);
+    winners.push(winner);
+  }
+  console.log(winners);
+  return winners;
+}
+
+function tournament(){
+  var contestents = setUpMatches();
+  if (tournySize === 8){
+    var roundOneWinners = tournamentRound(contestents);
+    var roundTwoWinners = tournamentRound(roundOneWinners);
+    var finalWinner = tournamentRound(roundTwoWinners);
+  } else if (tournySize === 16){
+    var roundOneWinners = tournamentRound(contestents);
+    var roundTwoWinners = tournamentRound(roundOneWinners);
+    var roudThreeWinners = tournamentRound(roundTwoWinners);
+    var finalWinner = tournamentRound(roundThreeWinners);
+  }
+  console.log('The final victor is: ' + finalWinner);
+}
