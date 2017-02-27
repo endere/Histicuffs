66ec48727c015384a0d8c684fc4b6728af7e30d8
Enrique Rico <enriquesantosrico@gmail.com>
2017-02-27 12:01:25 -0800

    worked on about us and css on that. on JS added teddy and leo

--- js/data.js
+++ js/data.js
@@ -8,10 +8,24 @@ var teddyQ3 = ['Both Roosevelts\'s wife and mother famously:', 'Died on the same
 
 var teddyQ4 = ['Teddy Roosevelt is the only President in American history to:', 'Win both the Medal of Honor and the Nobel Peace Prize', 'Fire his entire security cabinet on the same day', 'Survive four separate public assassination attempts', 'Shoot someone while in office'];
 
-// ADD questions above teddy var
+var teddyQ5 = ['Which of the following was Teddy Roosevelt NOT responible for during his presidency:', 'Engaging in a naval battle with China', 'Brokering peace between the Russians and Japanese', 'Creating America\'s first Nation Parks', 'Commissioning construction of the Panama Canal'];
+
+var teddyQ6 = ['What was the name of Roosevelt\'s newly formed political party during his third run for the presidency?', 'The Bull Moose Party', 'The New Progressives', 'The New Whigs', 'The Sons and Daughters of Liberty'];
+
+var teddyQ7 = ['What was one of Teddy Roosevelt\'s first hobbies as a child?', 'Taxidermy', 'Wrangling', 'Archery', 'Divination'];
+
+var teddyQ8 = ['Which of the following did Teddy Roosevelt NOT do?', 'Beat a would-be assassin to death with his cane', 'Go on a hunting expedition in Africa','Mapped a river system in the Amazon', 'Take his generals on fitness hikes because he thought they were too fat'];
+
+var teddyQ9 = ['Which vocation did Teddy Roosevelt NOT have in his lifetime?', 'Attorney General','Assistant Secretary of the Navy', 'Vice President', 'President'];
+
+var teddyQ10 = ['Teddy Roosevelt was a member of which political party during his presidency?', 'Republican', 'Democratic', 'Whig', 'Federalist'];
+
+var teddyQ11 = ['Teddy Roosevelt was:', 'The youngest person to ever be sworn in as a U.S. President.', 'The first President to serve more than two full terms.', 'The only person who hel the presidency who didn\'t drink.', 'Diagnosed with a pulmonary embolism at birth.'];
+
+var teddyQ12 = ['Which of the following was the name of one of Teddy Roosevelt\'s sons?', 'Adolf', 'Hubert', 'Kermit', 'Percivall'];
 
 var teddy = ['Teddy Roosevelt', 'img/Teddy.jpg', teddyQ1, teddyQ2, teddyQ3, teddyQ4, teddyQ5, teddyQ6, teddyQ7, teddyQ8, teddyQ9, teddyQ10, teddyQ11, teddyQ12];
-
+// Teddy Roosevelt Above!
 ///
 ///
 ///
@@ -26,6 +40,23 @@ var daVinciQ2 = ['Which one of these famous works of art was created by Da Vinci
 var daVinciQ3 = ['Where was Leonardo born?','Florence', 'Milan', 'Rome', 'Venice'];
 
 var daVinciQ4 = ['How many siblings did Da Vinci have?','17', '5', '0', '7'];
+
+var daVinciQ5 = ['In what year did Da Vinci leave Italy for good?', '1516', '1489', '1650', '1548'];
+
+var daVinciQ6 = ['In what year was Leonardo Da Vinci born?', '1452', '1992', '1534', '1628'];
+
+var daVinciQ7 = ['In what era was Da Vinci prominent in?', 'The Late Middle Ages', 'Atomic Age', 'Machine Age', 'Pre-History'];
+
+var daVinciQ8 = ['What was the name of Leonardo\'s mother?', 'Caterina', 'Lisa', 'Rose', 'Maria'];
+
+var daVinciQ9 = ['What did Leonardo do to study anatomy?', 'He opened corpses', 'He looked at his servants', 'He studied ancient medical books', 'He hired models'];
+
+var daVinciQ10 = ['Which of the following disgusted Leonardo?', 'Sexual Intercourse', 'Intestines', 'The Male Figure', 'Dead Rats'];
+
+var daVinciQ11 = ['About what age was Leonardo when he began his apprenticeship', '15', '13', '9','7'];
+
+var daVinciQ12 = ['What is the Mona Lisa called in Italian?', 'La Gioconda', 'La Mona lisa', 'La Mona Bella', 'La Benois Lisa'];
+
 //
 //ADD questions for daVinci var before this daVinci Var
 
