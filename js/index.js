8f13dc9e207a5b7e137a672494613d6a238583b9
Erik Enderlein <erik.end@gmail.com>
2017-03-02 11:30:38 -0600

    fixed index bug that did not allow user to input username or click on links

--- js/index.js
+++ js/index.js
@@ -3,15 +3,18 @@
 var usernameEl = document.getElementById('usernameForm');
 var userChoices = document.getElementById('userChoices');
 var playerChoice = '';
-
+var imgElements = document.getElementsByClassName('rosterImgs');
 usernameEl = addEventListener('submit', storeUsername);
-userChoices = addEventListener('click', setUserChoice);
+//userChoices = addEventListener('click', setUserChoice);
 
+for (var i = 0; i < imgElements.length; i++) {
+  imgElements[i].addEventListener('click', setUserChoice);
+}
 function storeUsername(event){
   event.preventDefault();
   event.stopPropagation();
 
-  var username = event.target.username.value;
+  var username = document.getElementById('inputName').value;
   localStorage.username = JSON.stringify(username);
   console.log(localStorage.username);
 }
@@ -19,11 +22,9 @@ function storeUsername(event){
 function setUserChoice(event){
   event.preventDefault();
   event.stopPropagation();
-  console.log(event);
-
-  if(event.target.className !== 'rosterImgs'){
-    alert('Please click on an image.');
-  } else {
+  if (event.target.className == 'formSubmit'){
+    storeUsername(event);
+  } else if(event.target.className == 'rosterImgs'){
     playerChoice = event.target.id;
     localStorage.playerChoice = JSON.stringify(playerChoice);
     console.log(localStorage.playerChoice);
