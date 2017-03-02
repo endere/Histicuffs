cc6b7ea70c77b215b6fc673eadf28def034a8900
Erik Enderlein <erik.end@gmail.com>
2017-03-02 16:44:39 -0600

    added win documents from Enrique

---
+++ win.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var user = JSON.parse(localStorage.username);
+
+var player = JSON.parse(localStorage.playerChoice);
+var playerIMG = [];
+var playerName = [];
+var lineUp = [];
+playerIMG.push(player);
+
+var photoHolder = document.getElementById('photoHolder');
+var photo = document.createElement('img');
+
+photo.setAttribute( 'src', playerIMG);
+photoHolder.appendChild(photo);
+
+var text = document.getElementById('text');
+var winMessage = document.createElement('p');
+winMessage.textContent = 'Congratulations ' + user + ' you have won using ';
+text.appendChild(winMessage);
